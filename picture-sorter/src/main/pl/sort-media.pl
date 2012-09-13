517e695ecd988b1d77b5430dff922e07bfa602c5
Edward Bridges <ebridges@squarespace.com>
2012-09-12 21:17:54 -0400

    cleaned up logging

--- picture-sorter/src/main/pl/sort-media.pl
+++ picture-sorter/src/main/pl/sort-media.pl
@@ -6,7 +6,8 @@ use warnings;
 use Time::Piece;
 use File::Copy;
 use File::Path;
-use Log::Log4perl;
+use Log::Log4perl qw(get_logger);
+use MediaFile;
 
 use constant ENABLED => 'yes';
 use constant REMOVE_ORIGINAL => undef;
@@ -18,6 +19,8 @@ chdir WD;
 
 Log::Log4perl->init(LOG_CONFIG);
 
+my $LOG = get_logger();
+
 # expect list of FQ filenames of source images.
 IMAGE: while(<>) {
     chomp;
@@ -28,42 +31,41 @@ IMAGE: while(<>) {
     my $valid = $mediaFile->validate();
 
     if($valid) {
-	get_logger()->info("image accepted for processing: [$image]");
+	$LOG->info("image accepted for processing: [$image]");
     } else {
-	get_logger()->warn("image type not accepted for processing: [$image]");
+	$LOG->warn("image type not accepted for processing: [$image]");
     }
 
     ## extract create date of image, formatted as yyyy-mm-dd
-    my $created = $mediaFile->create_date();
+    my $ok = $mediaFile->create_date();
 
-    if(not $created) {
-	get_logger()->warn("unable to extract create date for image [$image]");
+    if(not $ok) {
+	$LOG->warn("unable to extract create date for image [$image]");
 	next IMAGE;
     }
 
     my $dest_image = $mediaFile->format_dest_filepath(DEST);
 
-    get_logger()->logdie("dest image already exists! ($dest_image) from ($image)")
+    $LOG->logdie("dest image already exists! ($dest_image) from ($image)")
 	unless not -e $dest_image;
 
     if(ENABLED) {
 	my $successful;
-	get_logger()->info("copying [$image] to [$dest_image]");
-	$successful = $mediaFile->copyToDest($dest_image);
+	$LOG->info("copying [$image] to [$dest_image]");
+	$successful = $mediaFile->copy_to_dest($dest_image);
 
 	if(not $successful) {
-	    get_logger()->logdie("unable to copy [$image] to [$dest_image]: $!");
+	    $LOG->logdie("unable to copy [$image] to [$dest_image]: $!");
 	}
 
 	$successful = undef;
 	if(REMOVE_ORIGINAL) {
-	    get_logger()->info("removing source image [$image] after successful copy.");
+	    $LOG->info("removing source image [$image] after successful copy.");
 	    $successful = unlink $image;
+	    if(not $successful) {
+		$LOG->logdie("unable to remove source image [$image]: $!");
+	    }
 	}
-
-	if(not $successful) {
-	    get_logger()->logdie("unable to remove source image [$image]: $!");
-	}
+	$LOG->info("successfully copied [$image] to [$dest_image]");
     }
 }
-
