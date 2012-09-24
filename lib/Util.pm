34b0961a1931762db44bdf63c3d4a692481a6ba3
Edward Bridges <github@eqbridges.com>
2012-09-23 21:11:23 -0400

    converted away from Time::Piece to DateTime, added util functs

--- lib/Util.pm
+++ lib/Util.pm
@@ -3,7 +3,9 @@ package Util;
 use strict;
 use warnings;
 
-use Time::Piece;
+use DateTime::Format::ISO8601;
+use DateTime::Format::Strptime;
+
 use Log::Log4perl qw(get_logger);
 
 our $LOG = get_logger();
@@ -12,8 +14,45 @@ sub parse_date {
     my $date = shift;
     my @fmts  = @_;
     $LOG->trace("format_date($date, [@fmts]) called.");
-    my $t = Time::Piece->strptime($date, @fmts);
-    return $t;
+    
+    for(@fmts) {
+	next 
+	    unless $_;
+	my $fmt = DateTime::Format::Strptime->new( pattern => $_ );
+	my $d = $fmt->parse_datetime($date);
+	return $d
+	    if($d);
+    }
+
+    return undef;
+}
+
+# Takes two dates in ISO8601 format and returns a DateTime::Duration
+# object which represents their difference.  
+# Assumes param[0] is before param[1]
+sub date_diff {
+    my $dateStr1 = shift;
+    my $dateStr2 = shift;
+
+    my $dt1 = DateTime::Format::ISO8601->parse_datetime( $dateStr1 );
+    my $dt2 = DateTime::Format::ISO8601->parse_datetime( $dateStr2 );
+
+    return $dt2->subtract_datetime($dt1);
+}
+
+sub adjust_date {
+    my $dateOrig = shift;
+    my $duration = shift;    
+    my $copy = $dateOrig->clone();
+    my $adjusted = $copy->add_duration($duration);
+    return $adjusted;
+}
+
+sub is_before_now {
+    my $date = shift;
+    my $now = DateTime->now();
+    my $cmp = DateTime->compare( $date, $now );
+    return $cmp < 0;
 }
 
 sub trim {
