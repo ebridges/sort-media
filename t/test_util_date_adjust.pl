f644fc04a77e6442b714babba5548f38ab1126f6
Edward Bridges <ebridges@squarespace.com>
2012-10-25 20:10:26 -0400

    test for adjust semantics inverted

--- t/test_util_date_adjust.pl
+++ t/test_util_date_adjust.pl
@@ -4,23 +4,33 @@ use lib '../lib';
 
 use Util;
 
-use constant DATE_1 => '2034-04-30T16:14:00';
-use constant DATE_2 => '2012-09-04T23:20:00';
+use DateTime::Duration;
 
-my $diff = Util::date_diff(DATE_1, DATE_2);
+# 1y 1m 1w 1d 1h 1m 1s 0ns
+my $DURATION = DateTime::Duration->new(
+    years       => 1,
+    months      => 1,
+    weeks       => 0,
+    days        => 1,
+    hours       => 1,
+    minutes     => 1,
+    seconds     => 1,
+    nanoseconds => 0
+    );
 
-die "diff undef"
-    unless $diff;
+my $CASE_1_DATE      = DateTime::Format::ISO8601->parse_datetime( '2034-04-30T16:14:00' );
+my $CASE_1_EXPECTED  = DateTime::Format::ISO8601->parse_datetime( '2033-03-29T15:12:59' );
 
-my $date_1 =  DateTime::Format::ISO8601->parse_datetime( DATE_1 );
-my $date_2 =  DateTime::Format::ISO8601->parse_datetime( DATE_2 );
+my $CASE_2_DATE      = DateTime::Format::ISO8601->parse_datetime( '2034-04-02T20:59:39' );
+my $CASE_2_EXPECTED  = DateTime::Format::ISO8601->parse_datetime( '2033-03-01T19:58:38' );
 
-my $actual = Util::adjust_date( $date_1, $diff );
-my $expected = DateTime::Format::ISO8601->parse_datetime( DATE_2 );
+my $case1actual = Util::adjust_date($CASE_1_DATE, $DURATION);
+assert( (DateTime->compare($case1actual, $CASE_1_EXPECTED) == 0), 
+	"expected date [$CASE_1_EXPECTED] but was [$case1actual]");
 
-
-assert( (DateTime->compare($actual, $expected) == 0), 
-	"expected date [$expected] but was [$actual]");
+my $case2actual = Util::adjust_date($CASE_2_DATE, $DURATION);
+assert( (DateTime->compare($case2actual, $CASE_2_EXPECTED) == 0), 
+	"expected date [$CASE_2_EXPECTED] but was [$case2actual]");
 
 sub assert {
     my $bool = shift;
